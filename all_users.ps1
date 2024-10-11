cc8152fd3ddce24117775c036cdc1193f746c48e
Alex Bayir <103780136+alex-bayir@users.noreply.github.com>
2024-10-11 19:28:53 +0300

    remove imort module

--- all_users.ps1
+++ all_users.ps1
@@ -1,19 +1,13 @@
 param(
     [String]$out="all_users.csv",
     [string]$delimiter=";",
-    [string]$server,
-    [string]$ADCModule="Y:\Corps\СИБ\_ДИБ\Личные папки\Байрашный\ActiveDirectory"
+    [string]$server
 )
-
-if (!(Get-Module -All -Name *ActiveDirectory*)) {
-    Import-Module $ADCModule"\Microsoft.ActiveDirectory.Management.resources.dll"
-    Import-Module $ADCModule"\Microsoft.ActiveDirectory.Management.dll"
-    Import-Module $ADCModule"\ActiveDirectory.psd1"
-}
 
 function Fix-Name($name) {
     return $(if($name -is [String]) { $name.trim() -replace "  "," " } else { $null })
 }
+
 if($server){
     $users=Get-ADUser -Server $server -Filter * -SearchBase "DC=oek,DC=ru" -Properties SamAccountName,Enabled,CanonicalName,EmailAddress,Name,EmployeeID,Title,MemberOf
 }else{
@@ -37,4 +31,5 @@ $list=$users | ForEach-Object {
         groups = ($_.MemberOf | ForEach-Object { ($_ -replace "(CN|OU|DC)=","") -replace ",","/" }) -join "`n"
     }
 } | Sort-Object -Property account
+
 $list | Export-CSV $out -NoTypeInformation -Delimiter $delimiter -Encoding utf8
